9d9140baf1b3cf3c2d70d1f4d7ccd8391c52ad68
yuudong123 <1mouri81@gmail.com>
2025-06-15 10:30:47 +0900

    waste

--- hook.py
+++ hook.py
@@ -91,10 +91,34 @@ class Hook:
         waste1, _ = self.find_image('waste1', screen_img)
         waste2, _ = self.find_image('waste2', screen_img)
 
-        if waste1 or waste2:
-            return True
-        else:
-            return False
+        return waste1 or waste2
+
+    def waste_cancel(self):
+        found, match = self.find_image('is_fishing')
+        if found:
+            self.mouse.position = (match['center'][0], match['center'][1])
+            self.mouse.click(mouse.Button.left)
+        else: return
+        time.sleep(0.3)
+
+        found, match = self.find_image('cancel2')
+        if found:
+            self.mouse.position = (match['center'][0], match['center'][1])
+            self.mouse.click(mouse.Button.left)
+        else: return
+        time.sleep(1.7)
+
+        found, match = self.find_image('cancel1')
+        if found:
+            self.mouse.position = (match['center'][0], match['center'][1])
+            self.mouse.click(mouse.Button.left)
+        else: return
+        time.sleep(0.3)
+
+        found, match = self.find_image('cancel2')
+        if found:
+            self.mouse.position = (match['center'][0], match['center'][1])
+            self.mouse.click(mouse.Button.left)
 
     # 낚시중인지 체크
     def is_fishing(self):
--- main.py
+++ main.py
@@ -7,10 +7,16 @@ def main():
     hook = Hook(threshold=0.8)
     print('세팅 완료\n')
 
+    is_waste = False
+
     while True:
-        hook.start_hook()
-        print('낚시를 시작합니다')
-        sleep(1)
+        if not is_waste:
+            hook.start_hook()
+            print('낚시를 시작합니다')
+            sleep(1)
+        else:
+            hook.waste_cancel()
+            sleep(1.7)
         if hook.is_fishing():
             print('물고기 기다리기 시작')
             hook.wait_fish()
